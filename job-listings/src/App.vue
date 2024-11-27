16437ffe8a232f3aa1d65ec9ba6486568d16f574
CHEGEBB <chegephil24@gmail.com>
2024-11-27 16:37:04 +0300

    Refactor header size and update styling in App.vue

--- job-listings/src/App.vue
+++ job-listings/src/App.vue
@@ -1,213 +1,74 @@
 <template>
-<div class=" min-h-screen">
-  <div class="header bg-[#5ba4a4] m-0">
-    <!-- conditionally render and image depending on screen size if desktop or mobile -->
-    <img src="@/assets/images/bg-header-desktop.svg" alt="header" class="hidden md:block">
-    <img src="@/assets/images/bg-header-mobile.svg" alt="header" class="block md:hidden">
-
-  </div>
- <!-- Item Start -->
-  Photosnap
-  New!
-  Featured
-  Senior Frontend Developer
-  1d ago
-  Full Time
-  USA only
-  <!-- Role -->
-  Frontend
-  <!-- Level -->
-  Senior
-  <!-- Languages -->
-  HTML
-  CSS
-  JavaScript
-  <!-- Item End -->
-
-  <!-- Item Start -->
-  Manage
-  New!
-  Featured
-  Fullstack Developer
-  1d ago
-  Part Time
-  Remote
-  <!-- Role -->
-  Fullstack
-  <!-- Level -->
-  Midweight
-  <!-- Languages -->
-  Python
-  <!-- Tools -->
-  React
-  <!-- Item End -->
-
-  <!-- Item Start -->
-  Account
-  New!
-  Junior Frontend Developer
-  2d ago
-  Part Time
-  USA only
-  <!-- Role -->
-  Frontend
-  <!-- Level -->
-  Junior
-  <!-- Languages -->
-  JavaScript
-  <!-- Tools -->
-  React
-  Sass
-  <!-- Item End -->
-
-  <!-- Item Start -->
-  MyHome
-  Junior Frontend Developer
-  5d ago
-  Contract
-  USA only
-  <!-- Role -->
-  Frontend
-  <!-- Level -->
-  Junior
-  <!-- Languages -->
-  CSS
-  JavaScript
-  <!-- Item End -->
-
-  <!-- Item Start -->
-  Loop Studios
-  Software Engineer
-  1w ago
-  Full Time
-  Worldwide
-  <!-- Role -->
-  Fullstack
-  <!-- Level -->
-  Midweight
-  <!-- Languages -->
-  JavaScript
-  Ruby
-  <!-- Tools -->
-  Sass
-  <!-- Item End -->
-
-  <!-- Item Start -->
-  FaceIt
-  Junior Backend Developer
-  2w ago
-  Full Time
-  UK only
-  <!-- Role -->
-  Backend
-  <!-- Level -->
-  Junior
-  <!-- Languages -->
-  Ruby
-  <!-- Tools -->
-  RoR
-  <!-- Item End -->
-
-  <!-- Item Start -->
-  Shortly
-  Junior Developer
-  2w ago
-  Full Time
-  Worldwide
-  <!-- Role -->
-  Frontend
-  <!-- Level -->
-  Junior
-  <!-- Languages -->
-  HTML
-  JavaScript
-  <!-- Tools -->
-  Sass
-  <!-- Item End -->
-
-  <!-- Item Start -->
-  Insure
-  Junior Frontend Developer
-  2w ago
-  Full Time
-  USA only
-  <!-- Role -->
-  Frontend
-  <!-- Level -->
-  Junior
-  <!-- Languages -->
-  JavaScript
-  <!-- Tools -->
-  Vue
-  Sass
-  <!-- Item End -->
-
-  <!-- Item Start -->
-  Eyecam Co.
-  Full Stack Engineer
-  3w ago
-  Full Time
-  Worldwide
-  <!-- Role -->
-  Fullstack
-  <!-- Level -->
-  Midweight
-  <!-- Languages -->
-  JavaScript
-  Python
-  <!-- Tools -->
-  Django
-  <!-- Item End -->
-
-  <!-- Item Start -->
-  The Air Filter Company
-  Front-end Dev
-  1mo ago
-  Part Time
-  Worldwide
-  <!-- Role -->
-  Frontend
-  <!-- Level -->
-  Junior
-  <!-- Languages -->
-  JavaScript
-  <!-- Tools -->
-  React
-  Sass
-  <!-- Item End -->
+  <div class="min-h-screen">
+    <div class="header bg-[#5ba4a4] m-0">
+      <!-- Conditionally render an image depending on screen size -->
+      <img src="@/assets/images/bg-header-desktop.svg" alt="header" class="hidden md:block">
+      <img src="@/assets/images/bg-header-mobile.svg" alt="header" class="block md:hidden">
+    </div>
+    <div class="content">
+      <div v-for="job in jobs" :key="job.id" class="job">
+        <h2>{{ job.position }}</h2>
+        <p>{{ job.company }}</p>
+        <p>Location: {{ job.location }}</p>
+        <ul>
+          <li v-for="language in job.languages" :key="language">{{ language }}</li>
+        </ul>
+        <ul>
+          <li v-for="tool in job.tools" :key="tool">{{ tool }}</li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
-<script >
-
+<script>
 export default {
   name: 'App',
-  components: {
-  }
-}
+  data() {
+    return {
+      jobs: [], // Array to store all fetched jobs
+    };
+  },
+  mounted() {
+    const jobIds = Array.from({ length: 10 }, (_, i) => i);
+
+    // Fetch each job by ID
+    Promise.all(
+      jobIds.map((id) =>
+        fetch(`http://localhost:3000/${id}`).then((response) => response.json())
+      )
+    )
+      .then((data) => {
+        this.jobs = data; // Combine all fetched jobs into the `jobs` array
+      })
+      .catch((error) => console.error("Error fetching jobs:", error));
+  },
+};
 </script>
 
 <style lang="scss">
-$Desaturated-Dark-Cyan:hsl(180, 29%, 50%);
-$Background:hsl(180, 52%, 96%);
+$Desaturated-Dark-Cyan: hsl(180, 29%, 50%);
+$Background: hsl(180, 52%, 96%);
 $Filter-Tablets: hsl(180, 31%, 95%);
-$Dark-Grayish-Cyan:hsl(180, 8%, 52%);
-$Very-Dark-Grayish-Cyan:hsl(180, 14%, 20%);
-$font-league-spartan : 'League Spartan', sans-serif;
+$Dark-Grayish-Cyan: hsl(180, 8%, 52%);
+$Very-Dark-Grayish-Cyan: hsl(180, 14%, 20%);
+$font-league-spartan: 'League Spartan', sans-serif;
+
 * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
+
 #app {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   font-family: $font-league-spartan;
 }
-body{
+
+body {
   background: $Background;
   margin: 0;
   padding: 0;
-  
 }
 </style>
