c5a5f63600c7c4c019d5e45537aef2e306d72665
CHEGEBB <chegephil24@gmail.com>
2024-11-28 18:14:06 +0300

    Refactor job listings layout and styling in App.vue

--- job-listings/src/App.vue
+++ job-listings/src/App.vue
@@ -6,19 +6,29 @@
       <img src="@/assets/images/bg-header-mobile.svg" alt="header" class="block md:hidden">
     </div>
     <div class="content flex flex-col">
-      <div v-for="job in jobs" :key="job.id" class="job flex flex-row mx-auto my-0 mt-10 shadow-lg p-5 md:w-[70%] bg-white">
-         <div class="logo">
-    <img :src="getImagePath(job.logo)" alt="company logo" class="w-10 h-10" />
-  </div>
-  <div class="badges flex flex-row justify-between">
-    <div class="new" v-if="job.new">
-      <span class="badge">New</span>  
-    </div>
-    <div class="featured" v-if="job.featured">
-      <span class="badge">Featured</span>
-    </div>
-  </div>
-        <h2>{{ job.position }}</h2>
+      <div v-for="job in jobs" :key="job.id"
+        class="job md:flex md:flex-row sm:flex sm:flex-col mx-auto my-0 mt-10 shadow-lg p-5 md:w-[70%] bg-white">
+        <div class="logo">
+          <img :src="getImagePath(job.logo)" alt="company logo" class="w-10 h-10" />
+        </div>
+        <div class="details flex flex-col">
+          <div class="badges flex flex-row">
+            <div class="new" v-if="job.new">
+              <span class="badge">New</span>
+            </div>
+            <div class="featured" v-if="job.featured">
+              <span class="badge">Featured</span>
+            </div>
+          </div>
+          <div class="position">
+            <h2>{{ job.position }}</h2>
+          </div>
+          <div class="duration flex flex-row">
+            <p>{{job.postedAt}}</p>
+            <div class="dot-separator w-1 h-1 rounded-[50%] bg-slate-400">
+          </div>
+          </div>
+        </div>
         <p>{{ job.company }}</p>
         <p>Location: {{ job.location }}</p>
         <ul class="flex flex-row">
@@ -50,12 +60,12 @@ export default {
       )
     )
       .then((data) => {
-        this.jobs = data; 
+        this.jobs = data;
       })
       .catch((error) => console.error("Error fetching jobs:", error));
   },
-  methods :{
-    getImagePath(imageFileName){
+  methods: {
+    getImagePath(imageFileName) {
       return require(`@/assets/images/${imageFileName}`);
     },
   },
