26dbfa0750ada65247b3de9c5334b38b61962ded
CHEGEBB <chegephil24@gmail.com>
2024-12-01 13:45:55 +0300

    Refactor job listings layout and styling in App.vue, update .gitignore and vue.config.js, and add json-server dependency

--- job-listings/json-server/index.js
+++ job-listings/json-server/index.js
@@ -1,11 +1,10 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('data.json'); 
-const middlewares = jsonServer.defaults();
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('data.json')
+const middlewares = jsonServer.defaults()
 
-server.use(middlewares);
-server.use(router);
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log('JSON Server is running');
-});
+server.use(middlewares)
+server.use(router)
+server.listen(3001, () => {
+  console.log('JSON Server is running')
+})
