b337eaf7f273e6dd6a34d7d04f31ca8ba85f5a04
CHEGEBB <chegephil24@gmail.com>
2024-12-01 14:08:19 +0300

    Refactor backend server configuration and dependencies

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,10 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('data.json')
-const middlewares = jsonServer.defaults()
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('data.json');
+const middlewares = jsonServer.defaults({ static: false }); 
 
-server.use(middlewares)
-server.use(router)
+server.use(middlewares);
+server.use(router);
 server.listen(3001, () => {
-  console.log('JSON Server is running')
-})
+  console.log('JSON Server is running');
+});
