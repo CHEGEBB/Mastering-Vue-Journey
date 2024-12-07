1a5600095d7d34afbfdce68d5e0e9bc08a28c20a
CHEGEBB <chegephil24@gmail.com>
2024-12-07 13:44:25 +0300

    added template code

--- todo-app/src/App.vue
+++ todo-app/src/App.vue
@@ -1,16 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+
+<body>
+
+  Todo
+
+  <!-- Add dynamic number --> items left
+
+  All
+  Active 
+  Completed
+
+  Clear Completed
+
+  Drag and drop to reorder list
+</body>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
-
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
   }
 });
 </script>
