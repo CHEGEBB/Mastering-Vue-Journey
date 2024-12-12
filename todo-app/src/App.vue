0273d005abfe2a5e6e6309a7743324f3724aa1ea
CHEGEBB <chegephil24@gmail.com>
2024-12-12 22:49:38 +0300

    Refactor App.vue layout: Update todo input container styling and add text color to todo title

--- todo-app/src/App.vue
+++ todo-app/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="min-h-screen">
+  <div class="min-h-[120vh]">
     <div class="header">
       <img src="@/assets/images/bg-desktop-dark.jpg" alt="header" class="hidden md:block" v-show="isDarkTheme">
       <img src="@/assets/images/bg-desktop-light.jpg" alt="header" class="hidden md:block" v-show="!isDarkTheme">
@@ -7,7 +7,7 @@
       <img src="@/assets/images/bg-mobile-light.jpg" alt="header" class="block md:hidden w-full" v-show="isDarkTheme">
       <div class="top-[15%] absolute my-auto flex flex-row justify-between w-[37%] md:align-center left-1/2 transform -translate-x-1/2 -translate-y-1/2">
         <div class="logo">
-          <h1 class="uppercase text-3xl tracking-[12px] font-bold">Todo</h1>
+          <h1 class="uppercase text-3xl tracking-[12px] font-bold text-white">Todo</h1>
         </div>
         <div class="theme-switcher">
           <img src="@/assets/images/icon-sun.svg" alt="sun" class="hidden md:block sm:block" @click="toggleTheme" v-show="isDarkTheme" />
@@ -21,7 +21,7 @@
         <div class="w-5 h-5 border-2 border-[#4d5066] rounded-full cursor-pointer flex items-center justify-center absolute top-3 left-4 hover:border-[linear-gradient(hsl(192,_100%,_67%),_hsl(280,_87%,_65%))]" :class="{'bg-[linear-gradient(hsl(192,_100%,_67%),_hsl(280,_87%,_65%))]': isChecked}" @click="toggleChecked">
           <img v-if="isChecked" src="@/assets/images/icon-check.svg" alt="checkmark" class="w-3 h-3" />
         </div>
-        <div class="todo-body absolute top-[80px] w-[100%] rounded-md shadow-2xl" :class="isDarkTheme ? 'bg-[#25273c]' : 'bg-white'">
+        <div class="todo-body absolute top-[80px] w-[100%] rounded-md shadow-2xl text-lg" :class="isDarkTheme ? 'bg-[#25273c]' : 'bg-white'">
           <div v-for="todo in todo" :key="todo.id" class="todo-item flex flex-row justify-between w-[100%] p-4 focus:outline-none focus:ring-0 relative" :class="[
             isDarkTheme ? 'border-[#393a4c] text-[#fafafa]' : 'border-[#E3E4F1] text-[#494C6B]',
             'border-b-[1px]'
@@ -45,9 +45,9 @@
           </div>
         </div>
       </div>
-    </div>
-    <div class="text-center mt-8" :class="isDarkTheme ? 'text-[#5B5E7E]' : 'text-[#9495A5]'">
+      <div class="text-center mt-8" :class="isDarkTheme ? 'text-[#3445]' : 'text-[#9495A5]'">
       Drag and drop to reorder list
+    </div>
     </div>
   </div>
 </template>
