9a18a19dffb520124d50cacc32d99df1180c45ea
CHEGEBB <chegephil24@gmail.com>
2024-12-12 13:07:23 +0300

    Refactor App.vue layout: Update todo input container styling and add focus styles to input field

--- todo-app/src/App.vue
+++ todo-app/src/App.vue
@@ -35,7 +35,7 @@
   </div>
   <div class="todo-container top-[25%] absolute my-auto flex flex-col justify-between  w-[37%] md:align-center left-1/2  transform -translate-x-1/2 -translate-y-1/2">
     <div class="todo-input-container">
-    <input type="text" v-model="newTodo" placeholder="Create a new todo..." class="w-[100%] pl-12 p-3 rounded-md border-none bg-[#25273c]">
+    <input type="text" v-model="newTodo" placeholder="Create a new todo..." class="w-[100%] pl-12 p-3 rounded-md border-none bg-[#25273c] focus:outline-none focus:ring-0 text-[#fafafa]">
     <input type="radio" class="absolute left-4 top-4 " @change="addTodo">
   </div>
   <div class="bottom-nav">
@@ -85,6 +85,7 @@ export default defineComponent({
         }
       ],
     }
+    console.log('here')
     
   },
   methods:{
@@ -104,7 +105,7 @@ export default defineComponent({
     },
   },
   components: {
-  }
+  },
 });
 </script>
 
